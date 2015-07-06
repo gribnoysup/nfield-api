f0ccf91fdc5fd7b876a43b53bb64f953b56b0eac
sergeypetushkov <petushkov.sergey@gmail.com>
2015-07-06 13:14:17 +0000

    Add signIn() and configure() methods to Nfield module

--- nfieldapi.js
+++ nfieldapi.js
@@ -10,6 +10,36 @@ module.exports = (function Nfield () {
     var nfieldOptions = nfieldParams || {};
     var requestOptions = requestParams || {};
     
+    function updateParams (nP, rP) {
+      
+      nfieldOptions = nP || nfieldOptions;
+      requestOptions = rP || requestOptions;
+      
+      requestOptions.baseUrl = nfieldOptions.server || 'https://api.nfieldmr.com/';
+
+      request = Promise.promisify(require('request').defaults(requestOptions));
+      
+    }
+
+    function signIn (credentials, callback) {
+      return request({
+        method : 'post',
+        uri : 'v1/SignIn',
+        json : credentials
+      }).nodeify(callback);
+    }
+    
+    updateParams(nfieldOptions, requestOptions);
+    
+    return {
+      configure : function publicConfigure (nP, rP) {
+        updateParams(nP, rP);
+      },
+      signIn : function publicSignIn (cred, cb) {
+        return signIn(cred, cb);
+      }
+    };
+    
   }
   
   return {
