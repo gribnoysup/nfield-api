58e2af1eef4f321acbbe5a4273a554ad88c5146a
sergeypetushkov <petushkov.sergey@gmail.com>
2015-07-07 10:18:32 +0000

    Add jsDoc comments

--- nfieldapi.js
+++ nfieldapi.js
@@ -4,23 +4,46 @@ module.exports = (function Nfield () {
   
   var nfieldInstance;
   
+  /**
+   * Creates Nfield instance
+   * @param {Object} nfieldParams - Parameters to configure Nfield user to use with API, must contain server url and user credentials
+   * @param {Object=} requestParams - Parameters to configure request module, that is used to connect to API
+   */
   function createNfieldInstance (nfieldParams, requestParams) {
     
     var request;
     var nfieldOptions = nfieldParams || {};
     var requestOptions = requestParams || {};
     
+    /**
+     * Reconfigures Nfield instance
+     * @param {Object=} nP - Parameters to configure Nfield user to use with API, must contain server url and user credentials
+     * @param {Object=} rP - Parameters to configure request module, that is used to connect to API
+     */
     function updateParams (nP, rP) {
-      
       nfieldOptions = nP || nfieldOptions;
       requestOptions = rP || requestOptions;
-      
       requestOptions.baseUrl = nfieldOptions.server || 'https://api.nfieldmr.com/';
-
       request = Promise.promisify(require('request').defaults(requestOptions));
-      
     }
-
+    
+    /**
+     * Response callback
+     * @callback responseCallback
+     * @param {Error} Error
+     * @param {Object} Response
+     */
+    
+    /**
+     * Sign in to the Nfield API<br>
+     * {@link https://api.nfieldmr.com/help/api/post-v1-signin POST v1/SignIn API documentation}
+     * @param {Object} credentials - Sign in credentials
+     * @param {String} credentials.Domain
+     * @param {String} credentials.Username
+     * @param {String} credentials.Password
+     * @param {responseCallback} callback - Optional node-style callback
+     * @returns {Promise} Returns a promise of the request
+     */
     function signIn (credentials, callback) {
       return request({
         method : 'post',
@@ -28,7 +51,7 @@ module.exports = (function Nfield () {
         json : credentials
       }).nodeify(callback);
     }
-    
+
     updateParams(nfieldOptions, requestOptions);
     
     return {
@@ -42,13 +65,21 @@ module.exports = (function Nfield () {
     
   }
   
-  return {
-    init : function (nfieldParams, requestParams) {
-      if (!nfieldInstance) {
-        nfieldInstance = createNfieldInstance(nfieldParams, requestParams);
-      }
-      return nfieldInstance;
+  /**
+   * Creates new Nfield instance or returns existing one
+   * @param {Object} nfieldParams - Parameters to configure Nfield user to use with API, must contain server url and user credentials
+   * @param {Object=} requestParams - Parameters to configure request module, that is used to connect to API
+   * @returns {Object} Returns Nfield instance
+   */
+  function init (nfieldParams, requestParams) {
+    if (!nfieldInstance) {
+      nfieldInstance = createNfieldInstance(nfieldParams, requestParams);
     }
+    return nfieldInstance;
+  }
+  
+  return {
+    init : init
   };
   
 })();
