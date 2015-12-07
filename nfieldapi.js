08bfc53ed1ebb8841c5e9c3ec6974474ef422a0c
sergeypetushkov <petushkov.sergey@gmail.com>
2015-12-07 16:00:21 +0000

    Add 'defaults' function to NfieldClient

--- nfieldapi.js
+++ nfieldapi.js
@@ -9,13 +9,23 @@ var tokenUpdateTime = 12 * 60 * 1000;
  * Creates NfieldClient object
  * @constructor NfieldClient
  */
-function NfieldCliConstructor () {
+function NfieldCliConstructor (defOptions) {
   
   var defaultRequestCliOptions = {
     baseUrl : 'https://api.nfieldmr.com/',
     headers : {
       'Content-Type' : 'application/json'
     }
+  };
+  
+  extend(true, defaultRequestCliOptions, defOptions);
+  
+  /**
+   * Returns a new instance of NfieldClient with new request parameters
+   */
+  this.defaults = function defaults (options) {
+    if (typeof options !== 'object') throw new Error('`options` must be an object with request parameters');
+    return new NfieldCliConstructor(options);
   };
   
 }
