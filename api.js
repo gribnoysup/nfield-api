0ab0ce0d5d71e9b05af87108b2e1d0fec4004633
sergeypetushkov <petushkov.sergey@gmail.com>
2015-12-08 09:12:15 +0000

    Move Nfield API requests to separate file

---
+++ api.js
@@ -0,0 +1,110 @@
+var Promise = require('bluebird');
+var extend = require('extend');
+var request = Promise.promisify(require('request'));
+var tokenUpdateTime = 12 * 60 * 1000;
+
+/**
+ * SignIn to Nfield API
+ */
+function signIn (defOptions, credentials, callback) {
+  var options = {
+    method : 'POST',
+    uri : 'v1/SignIn',
+    json : credentials
+  };
+  
+  extend(true, options, defOptions);
+  return request(options).nodeify(callback);
+}
+
+/**
+ * Wrapper function for all Nfield API requests that checks if API token is not outdated, and refreshes it otherwise
+ */
+function requestWithTokenCheck (defOptions, credentials, token, options, callback) {
+  var returnedPromise;
+  var chainedPromise;
+  
+  extend(true, options, defOptions);
+  chainedPromise = request(options);
+  
+  if (Date.now() - token.Timestamp > tokenUpdateTime) {
+    returnedPromise = signIn(defOptions, credentials).then(function (data) {
+      if (data[0].statusCode !== 200) {
+        throw new Error(`${data[0].statusCode}: ${data[0].body.Message}`);
+      } else {
+        token.AuthenticationToken = data[0].body.AuthenticationToken;
+        token.Timestamp = Date.now();
+        return chainedPromise;
+      }
+    });
+  } else {
+    returnedPromise = chainedPromise;
+  }
+  
+  returnedPromise.nodeify(callback);
+  return returnedPromise;
+}
+
+/**
+ * Returns Nfield survey status
+ * 
+ * {@link https://api.nfieldmr.com/help/api/get-v1-surveys-surveyid-fieldwork-status}
+ */
+function getSurveyStatus (defOptions, credentials, token, surveyId, callback) {
+  var options = {
+    method : 'GET',
+    uri : `v1/Surveys/${surveyId}/Fieldwork/Status`,
+    headers : {
+      'Authorization' : `Basic ${token.AuthenticationToken}`
+    }
+  };
+  
+  return requestWithTokenCheck(defOptions, credentials, token, options, callback);
+}
+
+/**
+ * Starts Nfield survey
+ * 
+ * {@link https://api.nfieldmr.com/help/api/put-v1-surveys-surveyid-fieldwork-start}
+ */
+function startSurvey (defOptions, credentials, token, surveyId, callback) {
+  var options = {
+    method : 'PUT',
+    uri : `v1/Surveys/${surveyId}/Fieldwork/Start`,
+    headers : {
+      'Authorization' : `Basic ${token.AuthenticationToken}`
+    }
+  };
+  
+  return requestWithTokenCheck(defOptions, credentials, token, options, callback);
+}
+
+/**
+ * Stops Nfield survey
+ * 
+ * Has a 'magical' parameter called TerminateRunningInterviews, that never explained in any part of Nfield API documentation
+ * and doesn't seem to affect anything when changed, but present here for the sake of making requests as close to API docs as possible (defaults to 'false')
+ * 
+ * {@link https://api.nfieldmr.com/help/api/put-v1-surveys-surveyid-fieldwork-stop}
+ */
+function stopSurvey (defOptions, credentials, token, requestParams, callback) {
+  var options = {
+    method : 'PUT',
+    uri : `v1/Surveys/${requestParams.SurveyId}/Fieldwork/Stop`,
+    json : {
+      'TerminateRunningInterviews' : requestParams.TerminateRunningInterviews || false
+    },
+    headers : {
+      'Authorization' : `Basic ${token.AuthenticationToken}`
+    }
+  };
+  
+  return requestWithTokenCheck(defOptions, credentials, token, options, callback);
+}
+
+module.exports = {
+  signIn : signIn,
+  getSurveyStatus : getSurveyStatus,
+  startSurvey : startSurvey,
+  stopSurvey : stopSurvey
+};
--- index.js
+++ index.js
@@ -1,109 +1,7 @@
 "use strict";
 
-var Promise = require('bluebird');
+var API = require('./api.js');
 var extend = require('extend');
-var request = Promise.promisify(require('request'));
-var tokenUpdateTime = 12 * 60 * 1000;
-
-/**
- * Wrapper function for all Nfield API requests that checks if API token is not outdated, and refreshes it otherwise
- */
-function requestWithTokenCheck (defOptions, credentials, token, options, callback) {
-  var returnedPromise;
-  var chainedPromise;
-  
-  extend(true, options, defOptions);
-  chainedPromise = request(options);
-  
-  if (Date.now() - token.Timestamp > tokenUpdateTime) {
-    returnedPromise = SignIn(defOptions, credentials).then(function (data) {
-      if (data[0].statusCode !== 200) {
-        throw new Error(`${data[0].statusCode}: ${data[0].body.Message}`);
-      } else {
-        token.AuthenticationToken = data[0].body.AuthenticationToken;
-        token.Timestamp = Date.now();
-        return chainedPromise;
-      }
-    });
-  } else {
-    returnedPromise = chainedPromise;
-  }
-  
-  returnedPromise.nodeify(callback);
-  return returnedPromise;
-}
-
-/**
- * SignIn to Nfield API
- */
-function SignIn (defOptions, credentials, callback) {
-  var options = {
-    method : 'POST',
-    uri : 'v1/SignIn',
-    json : credentials
-  };
-  
-  extend(true, options, defOptions);
-  return request(options).nodeify(callback);
-}
-
-/**
- * Returns Nfield survey status
- * 
- * {@link https://api.nfieldmr.com/help/api/get-v1-surveys-surveyid-fieldwork-status}
- */
-function getSurveyStatus (defOptions, credentials, token, surveyId, callback) {
-  var options = {
-    method : 'GET',
-    uri : `v1/Surveys/${surveyId}/Fieldwork/Status`,
-    headers : {
-      'Authorization' : `Basic ${token.AuthenticationToken}`
-    }
-  };
-  
-  return requestWithTokenCheck(defOptions, credentials, token, options, callback);
-}
-
-
-/**
- * Starts Nfield survey
- * 
- * {@link https://api.nfieldmr.com/help/api/put-v1-surveys-surveyid-fieldwork-start}
- */
-function startSurvey (defOptions, credentials, token, surveyId, callback) {
-  var options = {
-    method : 'PUT',
-    uri : `v1/Surveys/${surveyId}/Fieldwork/Start`,
-    headers : {
-      'Authorization' : `Basic ${token.AuthenticationToken}`
-    }
-  };
-  
-  return requestWithTokenCheck(defOptions, credentials, token, options, callback);
-}
-
-/**
- * Stops Nfield survey
- * 
- * Has a 'magical' parameter called TerminateRunningInterviews, that never explained in any part of Nfield API documentation
- * and doesn't seem to affect anything when changed, but present here for the sake of making requests as close to API docs as possible (defaults to 'false')
- * 
- * {@link https://api.nfieldmr.com/help/api/put-v1-surveys-surveyid-fieldwork-stop}
- */
-function stopSurvey (defOptions, credentials, token, requestParams, callback) {
-  var options = {
-    method : 'PUT',
-    uri : `v1/Surveys/${requestParams.SurveyId}/Fieldwork/Stop`,
-    json : {
-      'TerminateRunningInterviews' : requestParams.TerminateRunningInterviews || false
-    },
-    headers : {
-      'Authorization' : `Basic ${token.AuthenticationToken}`
-    }
-  };
-  
-  return requestWithTokenCheck(defOptions, credentials, token, options, callback);
-}
 
 /**
  * Creates an instance of object, connected to Nfield API
@@ -114,9 +12,9 @@ function ConnectedInstance (requestOptions, authToken, credentials) {
   this.__CREDENTIALS = credentials;
   
   this.SurveyFieldwork = {
-    status : getSurveyStatus.bind(this, this.__REQUEST_OPTIONS, this.__CREDENTIALS, this.__TOKEN),
-    start : startSurvey.bind(this, this.__REQUEST_OPTIONS, this.__CREDENTIALS, this.__TOKEN),
-    stop : stopSurvey.bind(this, this.__REQUEST_OPTIONS, this.__CREDENTIALS, this.__TOKEN)
+    status : API.getSurveyStatus.bind(this, this.__REQUEST_OPTIONS, this.__CREDENTIALS, this.__TOKEN),
+    start : API.startSurvey.bind(this, this.__REQUEST_OPTIONS, this.__CREDENTIALS, this.__TOKEN),
+    stop : API.stopSurvey.bind(this, this.__REQUEST_OPTIONS, this.__CREDENTIALS, this.__TOKEN)
   };
 }
 
@@ -152,7 +50,7 @@ function NfieldCliConstructor (defOptions) {
     if (!credentials.Domain || !credentials.Username || !credentials.Password) throw new Error('not all required parameters provided: no `Domain` or `Username` or `Password`');
     
     var token = {};
-    var promise = SignIn(defaultRequestCliOptions, credentials).then(function (data) {
+    var promise = API.signIn(defaultRequestCliOptions, credentials).then(function (data) {
       if (data[0].statusCode !== 200) {
         throw new Error(`${data[0].statusCode}: ${data[0].body.Message}`);
       } else {
